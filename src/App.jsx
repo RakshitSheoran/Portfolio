ba82257482e78d083aab17dac91fa375c8c7c3ba
RakshitSheoran <rakshitsheoran8@gmail.com>
2026-03-26 22:37:07 +0530

    Font sizes and other responsive things

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import "./assets/styles/stylesheet.css";
 import GlassSurface from "./components/GlassSurface";
 import AppLayout from "./components/AppLayout";
-
+//This is the latest version//
 // import PillNav from "./components/CardNav";
 function App() {
   const [count, setCount] = useState(0);
